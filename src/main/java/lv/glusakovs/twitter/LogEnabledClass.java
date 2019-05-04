a813d019deb27ec1a4bcdaf13af5f4c153bc646d
reemsky <reemsky>
2019-05-04 23:16:45 +0300

    Updated spring-boot version

--- src/main/java/lv/glusakovs/twitter/LogEnabledClass.java
+++ src/main/java/lv/glusakovs/twitter/LogEnabledClass.java
@@ -1,7 +1,7 @@
 package lv.glusakovs.twitter;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public abstract class LogEnabledClass {
 	protected Logger log = LogManager.getLogger(this.getClass().getName());
