54727ed31c1f3bc8b203f75a575e950924d180aa
reemsky <reemsky>
2019-05-17 22:03:03 +0300

    Refactoring

--- src/main/java/lv/glusakovs/twitter/MessagePoster.java
+++ src/main/java/lv/glusakovs/twitter/MessagePoster.java
@@ -1,7 +1,6 @@
 package lv.glusakovs.twitter;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
@@ -19,50 +18,22 @@ public class MessagePoster extends LogEnabledClass {
 	@Autowired
 	private Config cfg;
 	private static int index = 0; // variable for message rotation
-
+	
 	public void post() {
-		List<String> messages;
-		String message;
 
 		TwitterAccount acc = cfg.getAccount();
 
 		if (!acc.isValid()) {
-			log.info(acc + "; valid:" + String.valueOf(acc.isValid()));
-			log.info("Account is invalid - skipping message");
+			log.error(acc + "; valid:" + String.valueOf(acc.isValid()));
+			log.info("Account is invalid. Message will not be posted.");
 			return;
 		}
 
 		log.info(acc.toString());
-		messages = cfg.getMessages();
-		message = messages.get(index);
-		String s;
-				
-		LocalDate date 	= LocalDate.now();
-		
-		String 	weekday 	= date.getDayOfWeek().toString();
-		String 	month 		= date.getMonth().toString();
-		int 	day 		= date.getDayOfMonth();
-		
-		s  = "Today is " + weekday;
-		s += ", " + month;
-		s += " " + day + ". "; 
-		
-		// Today is Day, DD of Month Year.  + message from list
-		String result = s + message;
-		log.info(result);
-	
-		String sha256 = "Not defined";
-		try {
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			byte[] digest = md.digest(result.getBytes(StandardCharsets.UTF_8));
-			sha256 = DatatypeConverter.printHexBinary(digest).toLowerCase();
-		} catch (NoSuchAlgorithmException e) {
-			log.error("Error getting SHA-256 MessageDigest instance");
-			e.printStackTrace();
-		}
-		log.debug("Message length is: " + result.length() + ", SHA-256: " + sha256);
-        
 
+		String msg = getMessageToPost();
+		log.info(msg);
+		log.debug("Message length is: " + msg.length() + ", SHA-256: " + getMessageSHA(msg));
 		/*
 		Twitter twitter = new TwitterTemplate(acc.getConsumerKey(), acc.getConsumerSecret(), acc.getAccessToken(),
 				acc.getAccessTokenSecret());
@@ -72,12 +43,39 @@ public class MessagePoster extends LogEnabledClass {
 			log.info("Twitter rejects duplicate message");
 		}
 		*/
-		nextMessage(messages.size() - 1);
+		
+		nextMessage();
 	}
 
-	private void nextMessage(int maxSize) {
+	private String getMessageToPost(){
+		String message 	= cfg.getMessages().get(index);
+		LocalDate date 	= LocalDate.now();
+		
+		String 	weekday 	= date.getDayOfWeek().toString();
+		String 	month 		= date.getMonth().toString();
+		int 	day 		= date.getDayOfMonth();
+		
+		// Message format: Today is DayOfWeek, DD of Month Year.  + message from list
+		String result = "Today is " + weekday + ", " + month + " " + day + ". " + message;
+		return result;
+	} 
+	
+	private String getMessageSHA(String s){
+		String sha256 = "Not defined";
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			byte[] digest = md.digest(s.getBytes(StandardCharsets.UTF_8));
+			sha256 = DatatypeConverter.printHexBinary(digest).toLowerCase();
+		} catch (NoSuchAlgorithmException e) {
+			log.error("Error getting SHA-256 MessageDigest instance");
+			e.printStackTrace();
+		}
+		return sha256;
+	}
+
+	private void nextMessage() {
 		index++;
-		if (index > maxSize) {
+		if (index > cfg.getMessages().size() - 1) {
 			index = 0;
 		}
 	}
